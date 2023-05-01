f018c64d4b3592b7bc201b071aabd40418e9a400
nefir2 <vadim.ahtamov@gmail.com>
2023-05-02 02:54:40 +0300

    find csc

--- src/main.cs
+++ src/main.cs
@@ -1,7 +1,7 @@
 namespace compilers {
 	class main {
 		public static void Main(string[] args) {
-			System.Console.WriteLine(args[0]);
-		}
+			System.Console.WriteLine(args[0]); //https://github.com/dotnet/roslyn
+        }
 	}
 }
