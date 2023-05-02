4058f2d59db8f954749edafad1987440dcbd9b75
nefir2 <vadim.ahtamov@gmail.com>
2023-05-02 12:58:57 +0300

    tkn

--- src/main.cs
+++ src/main.cs
@@ -1,7 +1,7 @@
 namespace compilers {
 	class main {
 		public static void Main(string[] args) { //must help: https://www.youtube.com/watch?v=J1ysuX4lIhc
-            System.Console.WriteLine(args[0]); //https://github.com/dotnet/roslyn
+            System.Console.WriteLine(args[0] + ":"); //https://github.com/dotnet/roslyn
         }
 	}
 }
