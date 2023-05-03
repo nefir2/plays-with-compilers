f0036659c758938661c6f3d740a6d178ecf7da86
nefir2 <vadim.ahtamov@gmail.com>
2023-05-03 11:47:10 +0300

    kali linux dev

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-using namespace std;
+//doesn't want build on kali linux...
 int main (int argc, char **argv) {
 	setlocale(0, "");
-	cout << "path to this program: " << argv[0] << "\n" << "args count: " << argc << "\nargs: ";
-	for (int i = 1; i < argc; i++) cout << "\"" << argv[i] << "\"" << (i != argc - 1 ? ", " : "");
+	std::cout << "path to this program: " << argv[0] << "\n" << "args count: " << argc << "\nargs: ";
+	for (int i = 1; i < argc; i++) std::cout << "\"" << argv[i] << "\"" << (i != argc - 1 ? ", " : "");
 	return 0;
 }
